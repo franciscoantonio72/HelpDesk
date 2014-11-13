38ccacbb7a7aeee7066f55adc1369886896e11a6
Francisco <francisco_72@hotmail.com>
2014-11-13 00:36:28 -0300

    Criacao do projeto de testes

---
+++ TestHelpDesk/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHelpDesk
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TimeSpan diferenca = Convert.ToDateTime("04/11/2014 08:00:00") - Convert.ToDateTime("03/11/2014 07:00:00");
+            double diferencaDias = diferenca.TotalDays;
+            double diferencaHoras = diferenca.TotalHours / 24;
+            double diferencaMinutos = diferenca.TotalMinutes;
+            double diferencaSegundos = diferenca.TotalSeconds;
+
+            Assert.AreEqual(1, diferencaDias);
+        }
+    }
+}
